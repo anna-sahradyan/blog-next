5ddf913666943eaefcaa99cca596941d1c8cfe5f
Anna Paruryan <a.sahradyan72@gmail.com>
2024-06-24 09:20:48 +0400

    Corrected adaptive menu burger

--- src/components/authComponent/AuthComponent.jsx
+++ src/components/authComponent/AuthComponent.jsx
@@ -1,10 +1,12 @@
 'use client'
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Link from "next/link";
 import s from './authLinks.module.css';
+import {ThemeContext} from "@/context/ThemeContext";
 
 const AuthComponent = () => {
     const [open, setOpen] = useState(false);
+    const {theme} = useContext(ThemeContext)
     useEffect(() => {
         const handleResize = () => {
             if (window.innerWidth > 640) {
@@ -40,7 +42,8 @@ const AuthComponent = () => {
                 <div className={s.line}></div>
             </div>
             {open && (
-                <div className={s.resMenu}>
+                <div className={s.resMenu}
+                     style={{ backgroundColor: theme === 'dark' ? '#0f172a' : '#F0F8FF' }}>
                     <Link href="/">Homepage</Link>
                     <Link href="/">About</Link>
                     <Link href="/">Contact</Link>
--- src/components/themeSwitch/ThemeSwitch.jsx
+++ src/components/themeSwitch/ThemeSwitch.jsx
@@ -10,10 +10,10 @@ const ThemeSwitch = () => {
     return (
         <div
             className={s.container} onClick={toggle}
-            style={theme==='dark'?{backgroundColor:"white"}:{backgroundColor:"#0f172a"}}
+            style={theme==='dark'?{backgroundColor:"#F0F8FF"}:{backgroundColor:"#0f172a"}}
         >
             <Image src="/img/moon.svg" alt="" width={14} height={14} />
-            <div className={s.ball}  style={theme==='dark'?{left:1,backgroundColor:"#0f172a"}:{right:1,backgroundColor:"white"}}></div>
+            <div className={s.ball}  style={theme==='dark'?{left:1,backgroundColor:"#0f172a"}:{right:1,backgroundColor:"#F0F8FF"}}></div>
             <Image src="/img/sun.svg" alt="" width={14} height={14} />
         </div>
     );
